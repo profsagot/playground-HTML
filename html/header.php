d0d96947f6c53c2d27035ba479e8a1437f46a9f9
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-09 14:00:49 +0100

    Update html/header.php

--- html/header.php
+++ html/header.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="UTF-8">
 		<?php
-			if (!empty(argv[2])) 
-				echo '<link rel="stylesheet" href="'.argv[2].'">';
+			if (!empty($argv[2])) 
+				echo '<link rel="stylesheet" href="'.$argv[2].'">';
 		?>
 		<title>Titre de page</title>
 	</head>
