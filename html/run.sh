4616f4f928e0dffd75105810aa7caf3ae153edce
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-11 17:08:57 +0100

    Update html/run.sh

--- html/run.sh
+++ html/run.sh
@@ -3,9 +3,9 @@ echo "TECHIO> redirect-streams 'Output'"
 php "header.php" $1".html" $3 > "output.html"
 
 
-cat $1".html"
+# cat $1".html"
 #ls ../target -al
 #ls ../workspace -al
 
-php Test.php $1".html" $2
+php Test.php "output.html" $2
 echo "TECHIO> open -s /project/target/ output.html"
